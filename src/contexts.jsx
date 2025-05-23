d6df2769df996f5a4930f662ab968a7cfc9ababe
nickbauman <nick.bauman@cirrusmachina.com>
2025-05-23 16:35:51 -0500

    use tanstack router

---
+++ src/contexts.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const hook = [[], function () {}];
+export const CartContext = createContext(hook);
---
+++ src/routes/__root.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Header from "../Header";
+import PizzaOfTheDay from "../PizzaOfTheDay";
+import { CartContext } from "../contexts";
+
+export const Route = createRootRoute({
+  component: () => {
+    const cartHook = useState([]);
+    return (
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
+});
