05b06920ac30fe72fb1e08a2d59675c859afc979
nickbauman <nick.bauman@cirrusmachina.com>
2025-05-26 15:18:53 -0500

    implement past orders (broken)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
-});
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 const App = () => {
-  const cartHook = useState([]);
-
   return (
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
---
+++ src/routes/past.lazy.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import getPastOrders from "../api/getPastOrders";
+
+export const Route = createLazyFileRoute("/past")({
+  component: PastOrdersRoute,
+});
+
+function PastOrdersRoute() {
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery({
+    queryKey: ["past-orders", page],
+    queryFn: () => getPastOrders(page),
+    staleTime: 1000 * 30,
+  });
+  if (isLoading) {
+    return (
+      <div className="past-orders">
+        <h2>Loading ...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="past-orders">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Date</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((order) => (
+            <tr key={order.order_id}>
+              <td>{order.order.id}</td>
+              <td>{order.order.date}</td>
+              <td>{order.order.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pages">
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <button disabled={data.length < 10} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
